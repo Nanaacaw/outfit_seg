9cbb8b88f44b9c9b6a33115f6cda35fbaaa90fbb
NanaCasmana <awnana123@gmail.com>
2025-07-04 09:41:14 +0700

    chore(setup): update setup.py with metadata and encoding

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='outfit_segmentation_app',
     version='0.1.0',
+    author='Nana Casmana Ade Wikarta',
+    description='API for Outfit Segmentation using FastAPI',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires= [requirement.strip() for requirement in open("requirements.txt").readlines()],
+    install_requires= [requirement.strip() for requirement in open("requirements.txt", encoding="utf-8").readlines()],
 )
