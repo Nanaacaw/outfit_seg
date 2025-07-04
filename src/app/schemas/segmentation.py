e698ee083d21d9af696cbead0143cd2381f109cf
NanaCasmana <awnana123@gmail.com>
2025-07-04 11:22:06 +0700

    fix(schema): support default labels and make labels optional

--- src/app/schemas/segmentation.py
+++ src/app/schemas/segmentation.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from typing import List, Optional
 
+# schema for image url
 class LabelRequest(BaseModel):
     image_url: str
-    labels: List[str]
-    threshold: Optional[float] = None
-    polygon_refinement: Optional[bool] = False
-    
+    labels: Optional[List[str]] = None # default is None
+    threshold: Optional[float] = None # default is None
+    polygon_refinement: Optional[bool] = False # default is False
