60802de515409446a2773465c891fc60c7f8aeb2
NanaCasmana <awnana123@gmail.com>
2025-07-08 11:13:16 +0700

    feat(schemas): use HttpUrl for validating image_url input

--- src/app/schemas/segmentation.py
+++ src/app/schemas/segmentation.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel, validator
+from pydantic import BaseModel,HttpUrl
 from typing import List, Optional
 
 # schema for image url
 class LabelRequest(BaseModel):
-    image_url: str
+    image_url: HttpUrl
     labels: Optional[List[str]] = None # default is None
     threshold: Optional[float] = None # default is None
     polygon_refinement: Optional[bool] = False # default is False
