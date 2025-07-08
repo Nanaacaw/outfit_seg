99e4acbb84fa103c02f4211d45926fad519793ec
NanaCasmana <awnana123@gmail.com>
2025-07-08 11:07:42 +0700

    feat(setting): add DEBUG_MODE config

--- src/app/settings/setting.py
+++ src/app/settings/setting.py
@@ -2,4 +2,7 @@
 DEFAULT_THRESHOLD= 0.3
 # Default model names
 DETECTOR_ID="IDEA-Research/grounding-dino-tiny"
-SEGMENTER_ID="facebook/sam-vit-large"
+SEGMENTER_ID="facebook/sam-vit-base"
+
+# Debug mode setting - set to True to show logging info, False to hide logging
+DEBUG_MODE = True
